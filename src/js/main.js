977ec16339508d40f3e0089e828b5e1c0672fb5b
Volodymy Yozh <k32skate@gmail.com>
2018-04-27 20:07:10 +0300

    star project

---
+++ src/js/main.js
@@ -0,0 +1,16 @@
+/*
+ * Third party
+ */
+//= ../../bower_components/jquery/dist/jquery.js
+
+//= ../../node_modules/slick-carousel/slick/slick.js
+
+//= ../../node_modules/owl.carousel/dist/owl.carousel.min.js
+
+var screenwidth = screen.width;
+
+
+console.log(screenwidth)
+
+
+//= partials/app.js
